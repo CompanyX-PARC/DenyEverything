b3569f23e0b1779812467656d51ab099ebdd0c51
Cody Jackson <codyjackson@csus.edu>
2015-02-26 16:21:43 -0800

    Committing first verification page.

---
+++ backend/Concordia/Concordia/Verify.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Concordia
+{
+    public partial class Verify : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submitButton_Click(object sender, EventArgs e)
+        {
+            if (verificationCode.Text.Equals("12345"))
+            {
+                group1.Attributes["class"] = "has-error form-group";
+                errorLabel.Text = "You are teh failz!";
+            }
+        }
+
+
+    }
+}
