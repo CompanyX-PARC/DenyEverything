34a63e02579e269a04300402a4658999a4c1bc69
Hilts <cpt.hilts@gmail.com>
2015-03-13 12:03:39 -0700

    roles and default users definated

--- backend/Concordia42/Concordia42/Migrations/Configuration.cs
+++ backend/Concordia42/Concordia42/Migrations/Configuration.cs
@@ -32,13 +32,31 @@ namespace Concordia42.Migrations
 
             // http://stackoverflow.com/questions/27498921/creating-asp-net-identity-user-in-seed-method-of-db-initializer
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
-            var userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            var User0 = new ApplicationUser() { FirstName = "student", LastName = "student", Email = "student@concordia.com", UserName = "student@concordia.com"};
+            var User1 = new ApplicationUser() { FirstName = "si", LastName = "si", Email = "si@concordia.com", UserName = "si@concordia.com" };
+            var User2 = new ApplicationUser() { FirstName = "staff", LastName = "staff", Email = "staff@concordia.com", UserName = "staff@concordia.com" };
+            var User3 = new ApplicationUser() { FirstName = "admin", LastName = "admin", Email = "admin@concordia.com", UserName = "admin@concordia.com" };
+
+            userManager.Create(User0, "concordia");
+            userManager.Create(User1, "concordia");
+            userManager.Create(User2, "concordia");
+            userManager.Create(User3, "concordia");
 
             roleManager.Create(new IdentityRole("admin"));
             roleManager.Create(new IdentityRole("assistant"));
             roleManager.Create(new IdentityRole("leader"));
             roleManager.Create(new IdentityRole("student"));
             roleManager.Create(new IdentityRole("verified"));
+
+            userManager.AddToRole(User0.Id, "student");
+            userManager.AddToRole(User1.Id, "leader");
+            userManager.AddToRole(User2.Id, "assistant");
+            userManager.AddToRole(User3.Id, "admin");
+
+
+
 
             context.Locations.AddOrUpdate(
                 l => l.locName,
