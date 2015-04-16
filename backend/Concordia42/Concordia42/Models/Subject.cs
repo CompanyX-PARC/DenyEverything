eb2828972dd2cc57bc6d5757417a2f55ef138197
Michael Garrett <michaelgarrett@csus.edu>
2015-04-16 10:37:28 -0700

    Saves it; commits it - en-database for subjects

---
+++ backend/Concordia42/Concordia42/Models/Subject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Concordia42.Models
+{
+    public class Subject
+    {
+        public Subject() 
+            //: base()
+        {
+
+        }
+
+        public int ID { get; set; }
+        public String Department { get; set; }
+        public String Number { get; set; }
+        public String Description { get; set; }
+
+        public virtual ICollection<ApplicationUser> Users { get; set; }
+
+    }
+}
