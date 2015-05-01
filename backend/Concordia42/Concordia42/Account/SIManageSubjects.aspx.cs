bd992f50d73251ca5dbef90596713a629891918d
Chris N <cneal116@gmail.com>
2015-05-01 16:06:14 -0700

    Database add now works.

--- backend/Concordia42/Concordia42/Account/SIManageSubjects.aspx.cs
+++ backend/Concordia42/Concordia42/Account/SIManageSubjects.aspx.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Concordia42.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Concordia42.Account
 {
@@ -16,7 +19,7 @@ namespace Concordia42.Account
 
         protected void czechUpdate(object sender, EventArgs e)
         {
-            
+            var db = new ApplicationDbContext();
             foreach (GridViewRow row in GridView2.Rows)
             {
                 if (row.RowType == DataControlRowType.DataRow)
@@ -30,13 +33,28 @@ namespace Concordia42.Account
                         //string number = row.Cells[1].Text;
                         int id = Convert.ToInt32(GridView2.DataKeys[row.RowIndex].Value);
                         //Response.Write(id);
+
+                      
+                        //db.SubjectUsers.Contains<>;
+                        SubjectUser bobDave = new SubjectUser();
+                        bobDave.UserID = User.Identity.GetUserId();
+                        bobDave.SubjectID = id;
+
+                        string myStupidUserIdStupidGummyDicks = User.Identity.GetUserId();
+                        if (!db.SubjectUsers.Any(s => s.SubjectID == id && s.UserID == myStupidUserIdStupidGummyDicks))
+                        {
+                            db.SubjectUsers.Add(bobDave);
+                        }
+                        
                     }
                     if (!chkRow.Checked)
                     {
                         row.BackColor = System.Drawing.Color.White;
                     }
                 }
-            }  
+            }
+            db.SaveChanges();
+
         }
     }
 }
